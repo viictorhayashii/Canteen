2d3e0cc1308c37d0e001eb744266780ca6871521
Victor Hayashi <viictorhayashi@outlook.com>
2021-05-18 16:54:42 -0300

    Tela Descricao Produto

---
+++ home/DescricaoProduto/assets/descricaoProduto.js
@@ -0,0 +1,10 @@
+const descricao = document.getElementById('descricao');
+const descricaoBanco = "X-salada Com Fritas"
+
+const valor = document.getElementById('valor');
+const valorBanco = "24,90";
+valor.textContent = "R$ " + valorBanco;
+
+
+
+descricao.textContent = descricaoBanco;
