caec50c9266145939ce318497e64f0ba879da4d5
Victor Hayashi <viictorhayashi@outlook.com>
2021-06-03 19:05:17 -0300

    ajustes login de adm

---
+++ home/Connection/Schemas/administrador.js
@@ -0,0 +1,34 @@
+import { OpenConnection } from "../connection.js";
+
+export default class Administrador
+{
+    salvarAdministrador(nome,senha,idAdministrador,idCanteen)
+    {
+        var nomeAdm = nome;
+        var senhaAdm = senha;
+        var idAdm = idAdministrador;
+        var idCantina = idCanteen;
+
+        var db = OpenConnection();
+        db.transaction(function(tx)
+        {
+            tx.executeSql('CREATE TABLE IF NOT EXISTS Administrador(nomeAdm,senhaAdm,idAdm unique,idCantina)');
+            tx.executeSql('INSERT INTO Administrador(nomeAdm,senhaAdm,idAdm,idCantina)' +
+            'values(?,?,?,?)', [nomeAdm, senhaAdm, idAdm, idCantina]);
+        });
+    }
+    consultarAdministrador(){
+        var db = OpenConnection();
+        var row = [];
+        db.transaction(function(tx) {
+            tx.executeSql('SELECT * FROM Administrador', [], function (tx, resultado) {
+                var len = resultado.rows.length;
+                for (var i=0; i<len; i++){
+                     row[i]= resultado.rows.item(i);
+                }                
+            });
+        });
+        return row;
+    }
+
+}
