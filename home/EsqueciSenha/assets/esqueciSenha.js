b88dd348acb89f571181a01ca2e08a8dda220c64
Victor Hayashi <viictorhayashi@outlook.com>
2021-06-06 13:15:05 -0300

    funcionalidade esqueci minha senha

--- home/EsqueciSenha/assets/esqueciSenha.js
+++ home/EsqueciSenha/assets/esqueciSenha.js
@@ -11,26 +11,32 @@ const validarUsuario  = () =>{
     const matricula = document.getElementById('matricula-esqueci').value;
     const mensagem = document.querySelector('.esqueci-senha');
     const error = document.querySelector('.error');
-    
+    var validado = false;
     for(var i = 0; i < listaUsuario.length; i++){
         var nomeValidado = listaUsuario[i].nome;
         var emailValidada = listaUsuario[i].email;
         var matriculaValidada = listaUsuario[i].matricula;
-        var validado = false;
+
        if(nome == nomeValidado && email == emailValidada && matricula == matriculaValidada ){
             mensagem.setAttribute('style', 'display: flex');
-            validado = true;
+            error.setAttribute('style', 'display: none');
+            limparCampos();
+            return validado = true;
        }
+
     }
     if(validado === false){
         error.setAttribute('style', 'display: flex');
+        mensagem.setAttribute('style', 'display: none');
+        limparCampos();
     }
-
-
-
     
-   
-    
+}
+
+function limparCampos(){
+    document.getElementById('nome-esqueci').value = '';
+    document.getElementById('email-esqueci').value = '';
+    document.getElementById('matricula-esqueci').value = '';
 }
 
 const validarLogin = document.querySelector('[data-form-button]');
