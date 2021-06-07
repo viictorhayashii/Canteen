1e998a58a5dd0229f2af2349d6f0c6e313103a21
Victor Hayashi <viictorhayashi@outlook.com>
2021-06-06 21:33:50 -0300

    ajustes

--- home/Connection/Schemas/produto.js
+++ home/Connection/Schemas/produto.js
@@ -1,21 +1,18 @@
 import { OpenConnection } from "../connection.js";
 
 
-export default class Produto 
-{
+export default class Produto {
     IdProduto = null;
     NomeProduto = null;
     DescricaoProduto = null;
     Categoria;
     PrecoProduto;
 
-    SaveProduto()
-    {
+    SaveProduto() {
         var db = OpenConnection();
         var idProd = 0;
         db.transaction(function(tx) {
-            tx.executeSql('SELECT * FROM Produto', [], function(tx, results)
-            {
+            tx.executeSql('SELECT * FROM Produto', [], function(tx, results) {
                 idProd = results.rows.length + 1;
             })
         });
@@ -23,49 +20,46 @@ export default class Produto
         var descProduto = this.DescricaoProduto;
         var categoria = this.Categoria;
         var precoProduto = this.PrecoProduto;
-        db.transaction(function(tx)
-        {
+        db.transaction(function(tx) {
             tx.executeSql('CREATE TABLE IF NOT EXISTS Produto(idProduto unique, nomeProduto TEXT,' +
                 'descricaoProduto TEXT, categoria TEXT, precoProduto TEXT)');
-            tx.executeSql('INSERT INTO Produto(idProduto, nomeProduto, descricaoProduto, categoria, precoProduto) '+
-            'values(?,?,?,?,?)', [idProd, nomeProduto, descProduto, categoria, precoProduto]);
+            tx.executeSql('INSERT INTO Produto(idProduto, nomeProduto, descricaoProduto, categoria, precoProduto) ' +
+                'values(?,?,?,?,?)', [idProd, nomeProduto, descProduto, categoria, precoProduto]);
         });
     }
 
-    deleteProduto(nomeProduto)
-    {
+    deleteProduto(nomeProduto) {
         var db = OpenConnection();
-        db.transaction(function (tx){
-            tx.executeSql('DELETE FROM Produto WHERE nomeProduto = ?', [nomeProduto]);
-            {
+        db.transaction(function(tx) {
+            tx.executeSql('DELETE FROM Produto WHERE nomeProduto = ?', [nomeProduto]); {
                 alert("Produto Excluido Com Sucesso");
             }
 
         });
     }
-    consultarProdutosPorCategoria(categoria){
+    consultarProdutosPorCategoria(categoria) {
         var db = OpenConnection();
         var row = [];
         db.transaction(function(tx) {
-            tx.executeSql('SELECT * FROM Produto WHERE categoria = ?',[categoria], function (tx, resultado) {
+            tx.executeSql('SELECT * FROM Produto WHERE categoria = ?', [categoria], function(tx, resultado) {
                 var len = resultado.rows.length;
-                for (var i=0; i<len; i++){
-                     row[i]= resultado.rows.item(i);
-                }                
+                for (var i = 0; i < len; i++) {
+                    row[i] = resultado.rows.item(i);
+                }
             });
         });
         return row;
     }
 
-    consultarProdutos(){
+    consultarProdutos() {
         var db = OpenConnection();
         var row = [];
         db.transaction(function(tx) {
-            tx.executeSql('SELECT * FROM Produto', [], function (tx, resultado) {
+            tx.executeSql('SELECT * FROM Produto', [], function(tx, resultado) {
                 var len = resultado.rows.length;
-                for (var i=0; i<len; i++){
-                     row[i]= resultado.rows.item(i);
-                }                
+                for (var i = 0; i < len; i++) {
+                    row[i] = resultado.rows.item(i);
+                }
             });
         });
         return row;
